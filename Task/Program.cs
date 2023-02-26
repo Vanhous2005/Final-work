b17ee8acf684763c2419a03249c06ca48a7a8a01
Vanhous2005 <v_dremov@list.ru>
2023-02-26 13:42:56 +0400

    Закончил решение задачи

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arr1 = new string[] {"h", "2", "world", "1234", "1567", "Russia"};
+string[] arr2 = new string[arr1.Length];
+
+void SecondArr(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if(arr1[i].Length <= 3)
+        {
+            arr2[count] = arr1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArr(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} "," ");
+    }
+    Console.WriteLine();
+}
+SecondArr(arr1, arr2);
+PrintArr(arr1);
+PrintArr(arr2);
